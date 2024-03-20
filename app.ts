32ea9eb1082e5e33189dd25ee01d1f4eee8431c0
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-21 01:43:47 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,11 @@
+// let allien_colour: string ="green";
+
+// if(allien_colour  == "green"){
+//     console.log("The player just earned 5 point");
+// }
+
+// allien_colour = "Red";
+
+// if(allien_colour  == "green"){
+//     console.log("The player just earned 5 point");
+// }
